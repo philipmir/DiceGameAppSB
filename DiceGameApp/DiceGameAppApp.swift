61e397aa520e08f221e0d923ca7cb41fcd1c752a
philipmir <philipmir@hotmail.com>
2023-04-04 11:16:14 +0200

    Initial Commit

---
+++ DiceGameApp/DiceGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGameAppApp.swift
+//  DiceGameApp
+//
+//  Created by Philip Miranda on 2023-04-04.
+//
+
+import SwiftUI
+
+@main
+struct DiceGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
